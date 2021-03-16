8d1a78bbfedf1a005ccd43164f78253cda507880
amoschoomy <acho0057@student.monash.edu>
2021-03-16 21:21:03 +0800

    Rollback to app before extra tasks

--- app/src/main/java/com/example/autoshowroom/MainActivity.java
+++ app/src/main/java/com/example/autoshowroom/MainActivity.java
@@ -18,13 +18,4 @@ public class MainActivity extends AppCompatActivity {
         EditText tv=findViewById(R.id.makertext);
         Toast.makeText(this, "Car maker: "+tv.getText().toString(), Toast.LENGTH_SHORT).show();
     }
-    public void resetField(View view){
-        EditText maker,model,year,color,seats,address,price;
-        maker=findViewById(R.id.makertext);model=findViewById(R.id.modeltext);
-        year=findViewById(R.id.yeartext);color=findViewById(R.id.colortext);
-        seats=findViewById(R.id.seattext); address=findViewById(R.id.addresstext);
-        price=findViewById(R.id.priceno);
-        maker.setText("");model.setText("");year.setText("");color.setText("");
-        seats.setText("");address.setText("");price.setText("");
-    }
 }
